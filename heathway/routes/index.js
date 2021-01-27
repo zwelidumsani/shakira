17ba2d387b4d8479dc4c0fb33b025f3080815db9
zwelidumsani <zwelidumsani@gmail.com>
2021-01-27 11:55:11 +0200

    Added All Remaining Pages

--- heathway/routes/index.js
+++ heathway/routes/index.js
@@ -6,4 +6,42 @@ router.get('/', function(req, res, next) {
   res.render('index.handlebars');
 });
 
+
+router.get('/electrical', function(req, res, next) {
+  res.render('electrical.handlebars');
+});
+
+router.get('/security', function(req, res, next) {
+  res.render('security.handlebars');
+});
+
+router.get('/construction', function(req, res, next) {
+  res.render('construction.handlebars');
+});
+
+
+router.get('/logistics', function(req, res, next) {
+  res.render('logistics.handlebars');
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about.handlebars');
+});
+
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact.handlebars');
+
+});
+
+
+router.get('/faq', function(req, res, next) {
+  res.render('faq.handlebars');
+
+});
+
+
+
+
+
 module.exports = router;
